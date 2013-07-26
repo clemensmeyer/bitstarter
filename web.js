b60c175b10bda9fdd2d5b89fde500b16c203d00a
Clemens Meyer <clemens.ludwig.meyer@gmail.com>
2013-07-26 13:49:45 +0000

    Third fix of web.js link to index.html

--- web.js
+++ web.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var file = "index.html";
 app.use(express.logger());
 
 /*app.get('/', function(request, response) {
@@ -7,16 +9,17 @@ app.use(express.logger());
 });
 */
 
-var websiteBuffer = new Buffer(8);
-websiteBuffer = fs.readFileSync(index.html);
+//var websiteBuffer = new Buffer(8);
+//websiteBuffer = fs.readFileSync(file);
 //var website
 //website = websiteBuffer.toString();
 app.get('/', function(request, response) {
-    response.send(websiteBuffer.toString());
+    var websiteBuffer = fs.readFileSync(file).toString;
+    response.send(websiteBuffer);
 });
 
-var port = process.env.PORT || 5000;
+/*var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-});
+});*/
 
