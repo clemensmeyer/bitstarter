aec56dce9fa3d9724f19a137a548f7b88b1385de
Clemens Meyer <clemens.ludwig.meyer@gmail.com>
2013-07-26 13:32:56 +0000

    Added reference from web.js to index.html

--- web.js
+++ web.js
@@ -2,8 +2,18 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-app.get('/', function(request, response) {
+/*app.get('/', function(request, response) {
   response.send('Hello World 2!');
+});
+*/
+
+var websiteBuffer = new Buffer(8);
+websiteBuffer = fs.readFileSync(index.html);
+var website
+
+website = websiteBuffer.toString();
+app.get('/', function(request, response) {
+    response.send(website);
 });
 
 var port = process.env.PORT || 5000;
